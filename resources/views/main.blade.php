1758e1a6c0a2d056bdb4148281164a9f078b1c78
Luis Lomeli <luism.lomeli@gmail.com>
2020-02-28 09:05:09 -0700

    Added container to embed testing applications

---
+++ resources/views/main.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+<div>
+	<br/>
+	<div>HERE YOU WILL SEE THE EMBEDDED APP</div>
+	
+	<br/>
+	<div>
+		<!-- Put here the embedded -->
+	</div>
+</div>
+@endsection
